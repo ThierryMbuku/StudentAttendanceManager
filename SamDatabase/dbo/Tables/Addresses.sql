74616a9a31ee91ca64f0032de08ad7cc07df2f45
THIERRY MBUKU BOBA <THIERRYM@eohmc.co.za>
2016-11-11 13:23:11 +0200

    -

---
+++ SamDatabase/dbo/Tables/Addresses.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Addresses] (
+    [ID]            INT           IDENTITY (1, 1) NOT NULL,
+    [ComplexNumber] INT           NOT NULL,
+    [Street]        NVARCHAR (50) NOT NULL,
+    [Suburb]        NVARCHAR (50) NOT NULL,
+    [City]          NVARCHAR (50) NOT NULL,
+    [PostalCode]    NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
---
+++ SamDatabase/dbo/Tables/Users.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [ID]           INT            IDENTITY (1, 1) NOT NULL,
+    [FirstName]    NVARCHAR (50)  NOT NULL,
+    [LastName]     NVARCHAR (50)  NOT NULL,
+    [Email]        NVARCHAR (100) NOT NULL,
+    [Username]     VARCHAR (50)   NOT NULL,
+    [PasswordHash] NVARCHAR (MAX) NOT NULL,
+    [CellPhone]    NVARCHAR (50)  NULL,
+    [IsAdmin]      BIT            NOT NULL,
+    [AddressID]    INT            NOT NULL,
+    CONSTRAINT [PK__Users__3214EC27195199F1] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [FK__Users__AddressID__29572725] FOREIGN KEY ([AddressID]) REFERENCES [dbo].[Addresses] ([ID])
+);
+
